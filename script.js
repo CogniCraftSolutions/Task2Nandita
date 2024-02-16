0b3ad1a9cb976f850e209d078b0d769c839848d6
Nandita-G <158209105+Nandita-G@users.noreply.github.com>
2024-02-16 10:54:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 let hrs=document.querySelector("#hours");
 let min=document.querySelector("#min");
 let sec=document.querySelector("#sec");
+let p=document.querySelector("p");
+let textCol=document.querySelector(".clock");
 
 setInterval(()=>{
     let date= new Date();
     hrs.innerHTML=(date.getHours()<10?"0":"")+date.getHours();
     min.innerHTML=(date.getMinutes()<10?"0":"")+date.getMinutes();
     sec.innerHTML=(date.getSeconds()<10?"0":"")+date.getSeconds();
+    if(date.getHours()>12){
+        p.innerHTML="pm";
+            }
+            else{
+                p.innerHTML="am";
+            }
 },1000);
+
+
 const container = document.querySelector('.main');
 
 function getRandomPosition() {
@@ -19,26 +29,29 @@ function getRandomPosition() {
 
   return { x: randomX, y: randomY };
 }
-const colorArray = ['red', 'yellow', 'green', 'pink'];
+ var colors=["red","green","yellow","cadetblue","navy","brown","coral"];
+ var index=0;
+ function changeColor(){
+  var randomIndex=Math.floor(Math.random()*colors.length);
+  var color=colors[randomIndex];
+  textCol.style.color=color;
+ }
+//  setInterval(()=>{
+//   changeColor();
+//  },1000);
 
-let colorIndex = 0;
+
+
 
 function moveContainer() {
   const newPosition = getRandomPosition();
   container.style.left = `${newPosition.x}px`;
   container.style.top = `${newPosition.y}px`;
-  container.style.transform = `translateZ(${Math.random() * 100}px)`;
-
-  const color = colorArray[colorIndex];
-container.style.backgroundColor = color;
-
-colorIndex = (colorIndex + 1) % colorArray.length;
-
-
-  container.style.animation = 'none'; 
-void container.offsetWidth; 
-container.style.animation = null; 
-
+  container.style.transform = `translateZ(${Math.random() * 80}px)`;
+   container.style.animation = 'none'; 
+  void container.offsetWidth; 
+  container.style.animation = null; 
+changeColor();
 
 }
-setInterval(moveContainer, 3000); 
+setInterval(moveContainer, 1000); 
